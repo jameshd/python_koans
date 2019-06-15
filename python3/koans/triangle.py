69093c351d360428fc2c70828de80b0d71b6ab4a
James HD <j_hattersleydykes@yahoo.co.uk>
2019-06-15 23:41:15 +0100

    progress

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -16,9 +16,25 @@
 # and
 #   about_triangle_project_2.py
 #
+
+
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    sides = set([a, b, c])
+
+    for side in sides:
+        if side <= 0:
+            raise TriangleError('All sides must be > 0')
+
+    if any([a+b <= c, a+c <= b, b+c <= a]):
+        raise TriangleError('The sum of any two sides should be \
+            greater than the third')
+                    
+    length = len(sides)
+
+    if length == 3:
+        return 'scalene'
+    return 'equilateral' if length == 1 else 'isosceles'
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
